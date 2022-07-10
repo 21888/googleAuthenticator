586c4e6beae8b6cb385844f8f75d940446f5c429
chabai <664485186@qq.com>
2022-07-10 16:37:45 +0800

    first commit

---
+++ example/main.go
@@ -0,0 +1,46 @@
+
+package main
+
+import (
+	"fmt"
+	"googleAuthenticator"
+)
+
+func createSecret(ga *googleAuthenticator.GAuth) string {
+	secret, err := ga.CreateSecret(16)
+	if err != nil {
+		return ""
+	}
+	return secret
+}
+
+func getCode(ga *googleAuthenticator.GAuth, secret string) string {
+	code, err := ga.GetCode(secret)
+	if err != nil {
+		return "*"
+	}
+	return code
+}
+
+func verifyCode(ga *googleAuthenticator.GAuth, secret, code string) bool {
+	// 1:30sec
+	ret, err := ga.VerifyCode(secret, code, 1)
+	if err != nil {
+		return false
+	}
+	return ret
+}
+
+func main() {
+	//if len(os.Args) != 2 {
+	//	return
+	//}
+	// secret := "LC42VPXL3VUMBCAN"
+	//secret := os.Args[1]
+	secret := "IU7B5Q3VBL55Q645"
+	ga := googleAuthenticator.NewGAuth()
+	fmt.Println(getCode(ga, secret))
+	// for i := 0; i < 10; i++ {
+	// 	log.Println(createSecret(ga))
+	// }
+}
